dd5f481c8d5687ccb32bab7ae2fedcfb940cb50a
Aria Cong <rongbehappy@gmail.com>
2025-04-08 22:47:44 +1000

    Adjest JSON viewer position

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
+import { TreeContextProvider } from './context/TreeContext'
+import TreeView from './components/TreeView'
+import JsonViewer from './components/JsonViewer'
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <Header />
+      <TreeContextProvider>
+        <TreeView />
+        <JsonViewer />
+      </TreeContextProvider>
     </div>
   )
 }
